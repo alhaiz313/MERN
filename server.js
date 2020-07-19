e064c05181a4ed6f009a27207ee179b80b0e9c35
zainabalhaidary <z.haidary@ymail.com>
2020-07-19 11:55:12 +0100

    created server and installed packages

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => res.send("API running")/*Sends data to browser*/);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server started on port " + PORT));
