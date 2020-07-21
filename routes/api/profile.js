025651f0883e307b233d644b0ff8a8f263668855
zainabalhaidary <z.haidary@ymail.com>
2020-07-21 14:44:05 +0100

    Del experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -196,4 +196,22 @@ router.put(
     }
   }
 );
+//@route Delete api/profile/experience
+//@desc Delete profile experience
+//@access Private
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    //get the remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIndex, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("Server error");
+  }
+});
 module.exports = router;
