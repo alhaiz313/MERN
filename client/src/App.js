32c9546f0a69fe994159ecce39ad8d04da65f056
zainabalhaidary <z.haidary@ymail.com>
2020-07-28 11:40:57 +0100

    load user and set auth token

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,37 @@
-import React, { Fragment } from "react";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-//redux
-import { Provider } from "react-redux";
-import store from "./store";
+import React, { Fragment, useEffect } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
 import Alert from './components/layout/Alert';
-
+import setAuthToken from './utils/setAuthToken';
+//redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+if (localStorage.token) {
+  //set the header with token if there is one
+  setAuthToken(localStorage.token);
+}
 const App = () => {
+  useEffect(() => {
+    // like componentDidMount in class Components
+    store.dispatch(loadUser());
+  }, []); //to make it only runs when mounted
+
   return (
     <Provider store={store}>
       <Router>
         <Fragment>
           <Navbar />
-          <Route exact path="/" component={Landing} />
-          <section className="container">
-            <Alert/>
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
             <Switch>
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/login" component={Login} />
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
             </Switch>
           </section>
         </Fragment>
