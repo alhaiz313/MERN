4e3ab2d9c1f9f4a4c76fc215665d4c9e62caa384
zainabalhaidary <z.haidary@ymail.com>
2020-07-28 12:28:48 +0100

    user logout functionality

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -6,6 +6,7 @@ import {
   AUTH_ERROR,
   LOGIN_FAIL,
   LOGIN_SUCCESS,
+  LOGOUT,
 } from './types';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthToken';
@@ -67,4 +68,9 @@ export const login = (email, password) => async (dispatch) => {
     }
     dispatch({ type: LOGIN_FAIL });
   }
+};
+
+//logout/clear profile
+export const logout = () => (dispatch) => {
+  dispatch({ type: LOGOUT });
 };
