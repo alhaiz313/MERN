ba8daf731a7006b931ea35ef942fec04a4bb2282
zainabalhaidary <z.haidary@ymail.com>
2020-07-29 12:53:01 +0100

    Edit profile

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import setAuthToken from './utils/setAuthToken';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/Edit-profile';
 //redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -40,6 +41,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
